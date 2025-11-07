5cd63100a062d98a2729aaa82fa8127f578ee045
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-11-07 15:50:54 +0600

    added googel auth provider

--- src/Contex/AuthProvider.jsx
+++ src/Contex/AuthProvider.jsx
@@ -1,11 +1,15 @@
 import {
   createUserWithEmailAndPassword,
+  GoogleAuthProvider,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signInWithPopup,
 } from "firebase/auth";
 import { AuthContext } from "./AuthContext";
 import { auth } from "../../firebase.config";
 import { useEffect, useState } from "react";
+
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
@@ -14,6 +18,11 @@ const AuthProvider = ({ children }) => {
   const createUserWithEmail = (email, password) => {
     setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
   };
 
   const signInWithEmail = (email, password) => {
