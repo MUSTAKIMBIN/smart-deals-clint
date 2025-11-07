9cf34179e87f9a642ab6b72bd8d909a1a21e9c30
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-11-07 15:22:40 +0600

    added authentication methods

--- src/Contex/AuthProvider.jsx
+++ src/Contex/AuthProvider.jsx
@@ -1,8 +1,40 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { AuthContext } from "./AuthContext";
+import { auth } from "../../firebase.config";
+import { useEffect, useState } from "react";
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUserWithEmail = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInWithEmail = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   const authInfo = {
-    mustakim: "mustakim",
+    createUserWithEmail,
+    signInWithEmail,
+    user,
+    loading,
   };
   return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
